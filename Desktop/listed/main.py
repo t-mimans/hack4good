4f811f501232e0dbcd950badab38daa1650c1c50
Eli Verbrugge <t-elverb@microsoft.com>
2020-06-27 16:02:04 -0500

    Configured dot-env

--- Desktop/listed/main.py
+++ Desktop/listed/main.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
 
-@app.route('/')
+app = Flask(__name__)
+load_dotenv()
+
+COGSVCS_KEY = os.getenv('COGSVCS_KEY')
+COGSVCS_CLIENTURL = os.getenv('COGSVCS_CLIENTURL')
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
     return 'Boo, World!'
