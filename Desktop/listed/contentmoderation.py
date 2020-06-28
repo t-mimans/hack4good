ddc94a3da9a4a8ee8fdafec6379cdf72ad224c07
Mike Kamwana <mikemula97@gmail.com>
2020-06-27 20:08:52 -0700

    added working content moderation

--- Desktop/listed/contentmoderation.py
+++ Desktop/listed/contentmoderation.py
@@ -12,6 +12,7 @@
 import os
 from pprint import pprint
 from dotenv import load_dotenv
+from io import BytesIO
 
 from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClient
 from azure.cognitiveservices.vision.contentmoderator.models import ( Screen )
@@ -25,38 +26,35 @@ SUBSCRIPTION_KEY = os.getenv('CONTENT_MODERATOR_SUBSCRIPTION_KEY')
 ENDPOINT         = os.getenv('CONTENT_MODERATOR_ENDPOINT')
 LOCATION         = os.getenv('CONTENT_MODERATOR_LOCATION')
 
-def text_moderation(filename):
+
+def text_moderation(text_to_moderate):
     '''
     This functon will moderate the input text.
-    @param text_to_screen, string --> text to be moderated
+    @param text, string --> text to be moderated
     '''
 
-    client = ContentModeratorClient( endpoint=ENDPOINT, credentials=CognitiveServicesCredentials(SUBSCRIPTION_KEY) )
+    client = ContentModeratorClient( endpoint=ENDPOINT, credentials=CognitiveServicesCredentials( SUBSCRIPTION_KEY ) )
+    
+    # Check for profanity in the input text
+    screen = client.text_moderation.screen_text(
+        text_content_type = "text/plain",
+        text_content = BytesIO(bytes(text_to_moderate, 'utf-8')),
+        language = "eng",
+        autocorrect = True,
+        pii = True,
+        classify = True
+    )
 
-    with open(filename, "rb") as text_fd:
-        
-        # Check for profanity in the input text
-        screen = client.text_moderation.screen_text(
-            text_content_type = "text/plain",
-            text_content = text_fd,
-            language = "eng",
-            autocorrect = True,
-            pii = True
-        )
-
-        assert isinstance(screen, Screen)
-        pprint(screen.as_dict())
-
-
-
-
+    assert isinstance(screen, Screen)
+    return screen.as_dict()
 
 
 def main():
     print("Hello World! \n\n")
 
     text = input().strip()
-    text_moderation(text)
+    pprint(text_moderation(text))
+
 
 if __name__ == "__main__":
     main()  
